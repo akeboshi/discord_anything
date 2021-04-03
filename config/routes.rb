0eb51b203324d2a6e43bdded21bd64c1d4a881e7
akeboshi <isao-aruga@bm-sms.co.jp>
2021-04-03 13:51:00 +0900

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :hoges
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/auth/:provider/callback', to: 'sessions#callback'
   post 'logout', to: 'sessions#logout'
 
-  resources :omikujis, only: %i[index create]
+  resources :omikujis, only: %i[index create destroy]
 end
