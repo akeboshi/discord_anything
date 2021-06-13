b6fcedeba9f8537203324fe7a8c4383d497d6698
akeboshi <isao-aruga@bm-sms.co.jp>
2021-06-13 15:19:33 +0900

    fix: oracle で複合uniqueが作れない

--- db/migrate/20210613023211_remove_custom_response_unique_index.rb
+++ db/migrate/20210613023211_remove_custom_response_unique_index.rb
@@ -1,6 +1,5 @@
 class RemoveCustomResponseUniqueIndex < ActiveRecord::Migration[6.1]
   def change
     remove_index :custom_responses, :key
-    add_index  :custom_responses, [:key, :response], unique: true
   end
 end
