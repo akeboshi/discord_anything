0c7a3553d6662d88161bd82d5ea538b89884e7fc
akeboshi <isao-aruga@bm-sms.co.jp>
2021-06-13 15:10:26 +0900

    fix

--- app/controllers/api/custom_responses_controller.rb
+++ app/controllers/api/custom_responses_controller.rb
@@ -9,6 +9,7 @@ class Api::CustomResponsesController < ApplicationController
                           .filter { |r| params[:word].include?(r.key) }
                           .group_by { |r| r.key }
                           .map{ |_, r| r[rand(r.length)]}
+                          .map(&:response)
     render json: { words: words }
   end
 end
