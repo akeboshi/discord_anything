6f3e279f14a9109e01d0fc07f6ee5f94007fd58e
akeboshi <isao-aruga@bm-sms.co.jp>
2021-06-13 17:50:12 +0900

    custom unique validate

--- app/models/custom_response.rb
+++ app/models/custom_response.rb
@@ -3,5 +3,11 @@ class CustomResponse < ApplicationRecord
 
   validates :key, presence: true, length: {maximum: 255, message: '反応する言葉は255文字以内にしてください'}
   validates :response, presence: true, length: {maximum: 1000, message: '返信内容は1000文字以内にしてください'}
-  validates :key, uniqueness: { scope: :response, message: '重複した反応がすでに登録されています' }
+  validate :key_response_unqique
+
+  def key_response_unqique
+    cr = CustomResponse.where(key: key)
+    cr = cr.filter { |r| r.response == response }
+    errors.add(:response, '重複した反応が既に登録されています') if cr.present?
+  end
 end
